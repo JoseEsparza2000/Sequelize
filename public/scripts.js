0e572da405a6a3aa39f74533a42d8d0fbbb417b8
Jose Esparza <73491225+JAEMANA@users.noreply.github.com>
2021-03-26 14:01:16 -0400

    update

---
+++ public/scripts.js
@@ -0,0 +1,21 @@
+async function fetch_data() {
+    const response = await fetch('/api/dining/');
+    const dining_halls = await response.json();
+    const myTable = document.querySelector('#myTable');
+    const data = dining_halls.data;
+  
+    data.forEach((item) => {
+      const row = data.map(item => {
+      return `
+          <tr>
+            <th>${item.hall_id}</th>
+            <th>${item.hall_name}</th>
+            <th>${item.hall_address}</th>
+          </tr>
+          `;
+      }).join('');
+  
+      myTable.innerHTML = row;
+    })
+  }
+  window.onload= fetch_data();
